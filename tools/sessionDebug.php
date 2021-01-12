620381ceb9548ca0c0afac4e8dbe82550b3e5374
= <=>
2021-01-12 17:44:56 -0300

    tools

--- tools/sessionDebug.php
+++ tools/sessionDebug.php
@@ -1,7 +1,5 @@
 <?php
-ini_set("xdebug.var_display_max_children", -1);
-ini_set("xdebug.var_display_max_data", -1);
-ini_set("xdebug.var_display_max_depth", -1);
+include 'displayError.php';
 
 
 function var_dump2($data, $label = '', $return = false)
